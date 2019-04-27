662e065a9de035daff1c7aa83a351d34c0b5b0a4
Charles Ross <chivalry@mac.com>
2019-04-27 11:23:04 -0700

    First working version

--- hours.py
+++ hours.py
@@ -3,6 +3,7 @@
 import click
 from datetime import datetime, date
 from calendar import monthrange, setfirstweekday
+import json
 
 
 MAX_HOURS_PER_WEEK = 30
@@ -28,10 +29,10 @@ def calculate_hours(hours, minutes, month):
     this_month = today.month
     year = today.year if month < this_month else today.year - 1
     start_day, month_days = monthrange(year, month)
-    used_week_hrs = hours * 60 + minutes
 
+    used_week_hrs = minutes_from_hrs_mins(hours, minutes)
     max_month = minutes_from_hrs_mins(MAX_HOURS_PER_MONTH, MAX_MINUTES_PER_MONTH)
-    used_week = minutes_from_hrs_mins(MAX_HOURS_PER_WEEK, MAX_MINUTES_PER_WEEK)
+    max_week = minutes_from_hrs_mins(MAX_HOURS_PER_WEEK, MAX_MINUTES_PER_WEEK)
 
     mins_per_day = max_month / month_days
     int_mins_per_day = int(mins_per_day)
@@ -39,30 +40,31 @@ def calculate_hours(hours, minutes, month):
     skip_days = int(month_days / extra_mins)
     mins_per_day = int_mins_per_day
 
-    hrs_per_day = int(mins_per_day / 60)
-    mins_per_day = mins_per_day - (hrs_per_day * 60)
-
     dates = [date(year, month, day) for day in range(1, month_days+1)]
-    daily_hours = [{'date': date, 'hrs': hrs_per_day, 'mins': mins_per_day} for date in dates]
+    daily_mins = [{'date': date, 'mins': mins_per_day} for date in dates]
 
     for i in range(extra_mins):
-        itr = i * skip_days
-        daily_hours[itr]['mins'] += 1
-        if daily_hours[itr] == 60:
-            daily_hrs[itr]['mins'] == 0
-            daily_hrs[itr]['hrs'] += 1
+        daily_mins[i * skip_days]['mins'] += 1
 
-    click.echo(str({
-        'max_month': max_month,
-        'hrs_per_day': hrs_per_day,
-        'mins_per_day': mins_per_day,
-        'extra_mins': extra_mins,
-        'start_day': start_day,
-        'skip_days': skip_days,
-        }))
+    start_week_days = (start_day * -1 - 1) % 7
+    mins = [daily_min['mins'] for daily_min in daily_mins]
+    first_week = used_week_hrs + sum(mins[:start_week_days])
+    i, j = 0, start_week_days + 0
+    while first_week > max_week:
+        daily_mins[i]['mins'] -= 1
+        daily_mins[j]['mins'] += 1
+        i = i + 1 if i + 1 < start_week_days else 0
+        j = j + 1 if j + 1 < month_days else start_week_days + 0
+        first_week -= 1
+
+    daily_times = []
+    for rec in daily_mins:
+        daily_times.append({'date': rec['date'],
+                            'hrs': rec['mins'] // 60,
+                            'mins': rec['mins'] % 60})
 
     result = ['{}: {} hrs, {} mins'.format(rec['date'], rec['hrs'], rec['mins'])
-            for rec in daily_hours]
+            for rec in daily_times]
     click.echo('\n'.join(result))
 
 if __name__ == '__main__':
